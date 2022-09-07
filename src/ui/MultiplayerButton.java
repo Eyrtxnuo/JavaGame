9194c632c02e0a56233567d384372afa1bb8077c
m1ncio <lorenzominciotti11@gmail.com>
2022-09-07 20:32:27 +0200

    host game button

--- src/ui/MultiplayerButton.java
+++ src/ui/MultiplayerButton.java
@@ -32,7 +32,7 @@ public class MultiplayerButton implements onClick {
     }
 
     private void loadImgs() {
-        imgs = new BufferedImage[2];
+        imgs = new BufferedImage[3];
         BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.MENU_BUTTONS);
         for (int i = 0; i < imgs.length; i++) {
             imgs[i] = temp.getSubimage(i * B_WIDTH_DEFAULT, rowIndex * B_HEIGHT_DEFAULT, B_WIDTH_DEFAULT, B_HEIGHT_DEFAULT);
